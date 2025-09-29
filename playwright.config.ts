bdd6027ca0a0369ffc540f6f207fe9866d086e4c
SuperNat <nmanlangit.it@gmail.com>
2025-09-29 15:29:16 +0800

    chore: temporary disable performance and accessibility tests (#341)
    * test: temporarily disabling performance and accessibility tests
    
    * test: updated to use glob pattern for locating accessibility and performance tests
    
    * test: added workaround for hover actions in navbar utility
    
    * test: added mouse move to center of the UI element to mimic playwright hover action
    
    * test: cleanup unrelated code change to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e',
+  /* Ignore perf and a11y tests for now */
+  testIgnore: ['**/performance.spec.ts', '**/accessibility.spec.ts'],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
